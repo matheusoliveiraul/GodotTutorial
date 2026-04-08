75c64ba17cd34e917859583250056050b4a8b46a
Matheus Soares <matheusoliveira.ul@gmail.com>
2026-04-08 16:10:04 -0300

    finalizei

--- Main.cs
+++ Main.cs
@@ -14,6 +14,9 @@ public partial class Main : Node
 		GetNode<Timer>("MobTimer").Stop();
 		GetNode<Timer>("ScoreTimer").Stop();
 		GetNode<Hud>("Hud").ShowGameOver();
+
+		GetNode<AudioStreamPlayer>("Music").Stop();
+    	GetNode<AudioStreamPlayer>("DeathSound").Play();
 	}
 
 	public void NewGame()
@@ -30,6 +33,8 @@ public partial class Main : Node
 		hud.UpdateScore(_score);
 		hud.ShowMessage("Get Ready!");
 		GetTree().CallGroup("mobs", Node.MethodName.QueueFree);
+
+		GetNode<AudioStreamPlayer>("Music").Play();
 	}
 
 	private void OnScoreTimerTimeout()
